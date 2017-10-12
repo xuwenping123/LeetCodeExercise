b44e08ab98fc6bad9789315cfcdc3f0e2be46a5b
徐文平 <xuwp@nfex.com>
2017-10-12 16:26:31 +0800

    [add] easy/RomanNumToInteger.java

---
+++ src/easy/RomanNumToInteger.java
@@ -0,0 +1,44 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Roman to Integer
+ * Given a roman numeral, convert it to an integer.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+public class RomanNumToInteger {
+
+    public int romanToInt(String s) {
+    	char[] array = s.toCharArray();
+    	int first = 0, second;
+    	int value = 0;
+    	
+    	Map<Character, Integer> map = new HashMap<Character, Integer>();
+    	map.put('I', 1);
+    	map.put('V', 5);
+    	map.put('X', 10);
+    	map.put('L', 50);
+    	map.put('C', 100);
+    	map.put('D', 500);
+    	map.put('M', 1000);
+    	
+    	for (int i = 0; i < array.length; i++) {
+    		second = map.get(array[i]);
+    		if (first < second && (first == 1 || first == 10 || first == 100)) {
+				value -= first;
+			} else {
+				value += first;
+			}
+    		first = second;
+    	}
+    	value += first;
+    	return value;
+    }
+    
+    public static void main(String[] args) {
+    	RomanNumToInteger num = new RomanNumToInteger();
+    	System.out.println(num.romanToInt("MCMXCVI"));
+    }
+}
