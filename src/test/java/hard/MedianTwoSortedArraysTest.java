67d9a0e96b404e822843d6de15a15657505fd31c
xuwenping <wenpingxu123@163.com>
2018-09-12 14:47:13 +0800

    4 median from two arrays

---
+++ src/test/java/hard/MedianTwoSortedArraysTest.java
@@ -0,0 +1,24 @@
+package hard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MedianTwoSortedArraysTest {
+
+    private MedianTwoSortedArrays sortedArrays = new MedianTwoSortedArrays();
+
+    @Test
+    public void testLotFindMedia() {
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        Assert.assertEquals(2, sortedArrays.findMedianSortedArrays(nums1, nums2), 0);
+        System.out.println(sortedArrays.findMedianSortedArrays(nums1, nums2));
+    }
+
+    @Test
+    public void testFindMedia() {
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4};
+        Assert.assertEquals(2.5, sortedArrays.findMedianSortedArrays(nums1, nums2), 0);
+    }
+}
