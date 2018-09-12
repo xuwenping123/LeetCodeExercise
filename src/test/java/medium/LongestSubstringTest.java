d7597d2122588de21386bbd901ebd6239f581d51
xuwenping <wenpingxu123@163.com>
2018-09-12 10:46:30 +0800

    3 longest subString count

---
+++ src/test/java/medium/LongestSubstringTest.java
@@ -0,0 +1,57 @@
+package medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LongestSubstringTest {
+
+    private LongestSubstring run = new LongestSubstring();
+
+    @Test
+    public void testNullString() {
+        String str = null;
+        Assert.assertEquals(0, run.lengthOfLongestSubstring(str));
+    }
+
+    @Test
+    public void testEmptyString() {
+        String str = "";
+        Assert.assertEquals(0, run.lengthOfLongestSubstring(str));
+    }
+
+    @Test
+    public void testOneCharString() {
+        String str = "a";
+        Assert.assertEquals(1, run.lengthOfLongestSubstring(str));
+    }
+
+    @Test
+    public void testTwoDiffCharString() {
+        String str = "ab";
+        Assert.assertEquals(2, run.lengthOfLongestSubstring(str));
+    }
+
+    @Test
+    public void testTwoSameCharString() {
+        String str = "aa";
+        Assert.assertEquals(1, run.lengthOfLongestSubstring(str));
+    }
+
+    @Test
+    public void testThreeCharString() {
+        String str = "aab";
+        Assert.assertEquals(2, run.lengthOfLongestSubstring(str));
+    }
+
+    @Test
+    public void testFourCharString() {
+        String str = "dedf";
+        Assert.assertEquals(3, run.lengthOfLongestSubstring(str));
+    }
+
+    @Test
+    public void testMoreCharString() {
+        String str = "abcabcbb";
+        Assert.assertEquals(3, run.lengthOfLongestSubstring(str));
+    }
+}
