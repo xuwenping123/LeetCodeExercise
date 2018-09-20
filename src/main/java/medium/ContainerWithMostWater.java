c31f3d02431bf2f05bb0abec83e24c1fac2decaf
xuwenping <wenpingxu123@163.com>
2018-09-20 10:52:56 +0800

    submit success

--- src/main/java/medium/ContainerWithMostWater.java
+++ src/main/java/medium/ContainerWithMostWater.java
@@ -2,9 +2,29 @@ package medium;
 
 public class ContainerWithMostWater {
 
-
+    /**
+     * 使用枚举法，遍历过程中有效剔除
+     * @param height
+     * @return
+     */
     public int maxArea(int[] height) {
-
-        return 0;
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+        int temp;
+        int value = 0;
+        for (int i = 0; i < height.length - 1; i++) {
+            temp = (height.length - 1 - i) *
+                    (height[i] < height[height.length - 1] ? height[i] : height[height.length - 1]);
+            value = temp > value ? temp : value;
+            for (int j = height.length - 1; j > i + 1; j--) {
+                if (height[j] < height[j - 1]) {
+                    temp = height[i] < height[j - 1] ? height[i] : height[j - 1];
+                    temp = (j - 1 - i) * temp;
+                    value = temp > value ? temp : value;
+                }
+            }
+        }
+        return value;
     }
 }
--- src/test/java/medium/ContainerWithMostWaterTest.java
+++ src/test/java/medium/ContainerWithMostWaterTest.java
@@ -1,11 +1,23 @@
 package medium;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ContainerWithMostWaterTest {
 
     private ContainerWithMostWater container = new ContainerWithMostWater();
 
 
+    @Test
+    public void testMaxArea() {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        Assert.assertEquals(49, container.maxArea(height));
+    }
 
-
+    @Test
+    public void testMax2Area() {
+        int[] height = {1,8,7};
+        Assert.assertEquals(7, container.maxArea(height));
+    }
 }
