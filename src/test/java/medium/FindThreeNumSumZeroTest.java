46a4156b35abd58d5870120cb75ac59818cb81af
xuwenping <wenpingxu123@163.com>
2018-09-28 10:26:11 +0800

    remove nouse import

--- src/test/java/medium/FindThreeNumSumZeroTest.java
+++ src/test/java/medium/FindThreeNumSumZeroTest.java
@@ -3,9 +3,7 @@ package medium;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class FindThreeNumSumZeroTest {
