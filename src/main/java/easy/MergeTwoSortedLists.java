55b5a6181976439431b9c17c25d844e1cecc9cf7
xuwenping <wenpingxu123@163.com>
2018-09-29 10:55:00 +0800

    solve sort list merge

--- src/main/java/easy/MergeTwoSortedLists.java
+++ src/main/java/easy/MergeTwoSortedLists.java
@@ -11,22 +11,53 @@ package easy;
 public class MergeTwoSortedLists {
 
 	/**
-	 * 
+	 * 借助归并排序算法数组合并思想进行处理
+	 * @param l1
+	 * @param l2
+	 * @return
 	 */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return null;
+		if (l1 == null) {
+			return l2;
+		}
+		if (l2 == null) {
+			return l1;
+		}
+		ListNode node = new ListNode(-1);
+		ListNode head = node;
+		while (l1 != null && l2 != null) {
+			if (l1.val <= l2.val) {
+				node.next = l1;
+				l1 = l1.next;
+			} else {
+				node.next = l2;
+				l2 = l2.next;
+			}
+			node = node.next;
+		}
+		if (l1 != null) {
+			while (l1 != null) {
+				node.next = l1;
+				node = node.next;
+				l1 = l1.next;
+			}
+		} else if (l2 != null) {
+			while (l2 != null) {
+				node.next = l2;
+				node = node.next;
+				l2 = l2.next;
+			}
+		}
+        return head.next;
     }
-    
-    public static void main(String[] args) {
-		
+
+
+	static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+		}
 	}
 }
 
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode(int x) {
-		val = x;
-	}
-}
---
+++ src/test/java/easy/MergeTwoSortedListsTest.java
@@ -0,0 +1,21 @@
+package easy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergeTwoSortedListsTest {
+
+    private MergeTwoSortedLists sorted;
+
+    @Before
+    public void setUp() throws Exception {
+        sorted = new MergeTwoSortedLists();
+    }
+
+    @Test
+    public void mergeTwoLists() {
+
+    }
+}
