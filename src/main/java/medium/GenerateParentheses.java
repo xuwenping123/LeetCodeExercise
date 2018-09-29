969cc04d8b6e0d13db12aeba63abc59e395738f5
xuwenping <wenpingxu123@163.com>
2018-09-29 15:02:19 +0800

    solve

--- src/main/java/medium/GenerateParentheses.java
+++ src/main/java/medium/GenerateParentheses.java
@@ -1,9 +1,8 @@
 package medium;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Stack;
-
 /**
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  *
@@ -19,48 +18,21 @@ import java.util.Stack;
  */
 public class GenerateParentheses {
 
+    /**
+     * copy from
+     * @param n
+     * @return
+     */
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<String>();
-        StringBuilder sb = new StringBuilder();
-        List<Character> list = new ArrayList<Character>();
-        for (int i = 0; i < n; i++) {
-            list.add('(');
+        List<String> ans = new ArrayList<String>();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c)
+                for (String left: generateParenthesis(c))
+                    for (String right: generateParenthesis(n-1-c))
+                        ans.add("(" + left + ")" + right);
         }
-        for (int i = 0; i < n; i++) {
-            list.add(')');
-        }
-        generate(result, 0, list, sb);
-        return result;
-    }
-
-    private void generate(List<String> result, int index, List<Character> list, StringBuilder sb) {
-        if (index >= list.size()) {
-            String str = sb.toString();
-            if (isParenthesis(str)) {
-                result.add(str);
-            }
-            return;
-        }
-        for (int i = index; i < list.size(); i++) {
-            sb.append(list.get(i));
-            generate(result, index + 1, list, sb);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-    }
-
-    private boolean isParenthesis(String sb) {
-        Stack<Character> stack = new Stack<Character>();
-        for (int i = 0; i < sb.length(); i++) {
-            if (stack.empty()) {
-                stack.push(sb.charAt(i));
-                continue;
-            }
-            if (stack.peek() == '(' && sb.charAt(i) ==')') {
-                stack.pop();
-            } else {
-                stack.push(sb.charAt(i));
-            }
-        }
-        return stack.empty();
+        return ans;
     }
 }
