c129a8a4e27a019ac46a26d08a2066b60719ad78
会抓猫的鱼 <wenpingxu123@163.com>
2020-03-30 15:40:25 +0800

    1. 添加add Strings

--- src/main/java/easy/AddStrings.java
+++ src/main/java/easy/AddStrings.java
@@ -21,11 +21,14 @@ public class AddStrings {
         }
         char[] p = num1.toCharArray();
         char[] q = num2.toCharArray();
+        // 两个数相加，新生成的元素的数组的长度最多+1，即可保证可容纳
         char[] result = new char[p.length > q.length ? p.length + 1 : q.length + 1];
         int pIndex = p.length - 1, qIndex = q.length - 1;
         int index = result.length;
         int carry = 0;
         int sum;
+        // 元素计算时从最后一位计算
+        // 进入结果数组时也从最后一位入组
         while (pIndex >= 0 || qIndex >= 0) {
             int x = (pIndex < 0) ? 0 : (p[pIndex] - '0');
             int y = (qIndex < 0) ? 0 : (q[qIndex] - '0');
